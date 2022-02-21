dc3865090bed52bac0318634ea20ae7b71121cf5
Alan85000 <mathealan@gmail.com>
2022-02-21 09:29:38 +0100

    Ajoute exercice 7

---
+++ formulaireexosept.php
@@ -0,0 +1,16 @@
+<?php error_reporting(E_ALL & ~E_NOTICE); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 7</title>
+</head>
+<body>
+<a href="menuexosept.php?Message=Vous%20allez%20manger%20une%20pizza" title="Envoyer">Pizza</a>
+<a href="menuexosept.php?Messagedeux=Vous%20allez%20manger%20de%20la%20salade" title="Envoyerdeux">Salade</a>
+<a href="menuexosept.php?Messagetrois=Vous%20allez%20manger%20du%20poisson" title="Envoyertrois">Poisson</a>
+<a href="menuexosept.php?Messagequatre=Vous%20allez%20manger%20de%20la%20viande" title="Envoyerquatre">Viande</a>
+</body>
+</html>
